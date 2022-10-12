27765db4412f0cef096f415405d527e2bbcf84eb
MAZE Choiji <113877093+choiji0513@users.noreply.github.com>
2022-10-12 21:19:55 +0900

    add SVG

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
 import { RestaruantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+import { theme } from './src/infrastructure/theme';
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import {
+  useFonts as useLato,
+  Lato_400Regular,
+} from '@expo-google-fonts/lato';
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if(!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
-      <RestaruantsScreen/>
+      <ThemeProvider theme={ theme }>
+        <RestaruantsScreen/>
+      </ThemeProvider>
       <ExpoStatusBar style='auto' />
     </>
   );
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,6 +2,8 @@ import React from "react";
 import { StyleSheet, Text } from "react-native";
 import { Card } from "react-native-paper";
 import styled from 'styled-components/native'
+import { SvgXml } from 'react-native-svg';
+import star from "../../../../assets/star";
 
 export const RestaruantInfoCard = ({ restaurant = {} }) => {
   const {  
@@ -16,24 +18,45 @@ export const RestaruantInfoCard = ({ restaurant = {} }) => {
     isClosedTemporarily,
   } = restaurant
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+  console.log(ratingArray)
   return (
-    <Card elevation={ 5 } style={ styles.card }>
-      <Card.Cover key={ name } style={ styles.cover } source={{ uri:photos[0] }} />
-      <Title>{ name }</Title>
-    </Card>
+    <RestaruantCard elevation={ 5 }>
+      <RestaruantCardCover key={ name } source={{ uri:photos[0] }} />
+      <Info>
+        <Title>{ name }</Title>
+        <Rating>
+        {ratingArray.map(() =><SvgXml xml={ star } width={ 20 } height={ 20 }/> )}
+        </Rating>
+        <Address>{ address }</Address>
+      </Info>
+    </RestaruantCard>
   )
 }
 
-const Title = styled.Text`
-  padding: 16px;
+const RestaruantCard = styled(Card)`
+  background-color: white;
+  background-color: ${ (props) => props.theme.colors.bg.primary };
 `
-
-const styles = StyleSheet.create({
-  card:{
-    backgroundColor:'white'
-  },
-  cover:{
-    padding: 20,
-    backgroundColor:'white'
-  },
-})
+const RestaruantCardCover = styled(Card.Cover)`
+  padding: ${ (props) => props.theme.space[3] };
+  background-color: ${ (props) => props.theme.colors.bg.primary };
+`
+const Info = styled.View`
+  padding: ${ (props) => props.theme.space[3] };
+`
+const Rating =  styled.View`
+  flex-direction: row;
+  padding-top: ${ (props) => props.theme.space[2] };
+  padding-bottom: ${ (props) => props.theme.space[2] };
+`
+const Title = styled.Text`
+  font-family: ${ (props) => props.theme.fonts.heading };
+  font-size: ${ (props) => props.theme.fontSizes.body };
+  color: ${ (props) => props.theme.colors.ui.primary };
+`
+const Address = styled.Text`
+  font-family: ${ (props) => props.theme.fonts.body };
+  font-size: ${ (props) => props.theme.fontSizes.body  };
+  color: ${ (props) => props.theme.colors.ui.primary };
+`
